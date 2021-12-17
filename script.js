56a5227184595b9b16213749fe22c64472c7fa52
alvmnt <araalvarezmo@unal.edu.co>
2021-12-17 09:54:26 -0500

    Refactor for incorporating UI

--- script.js
+++ script.js
@@ -1,61 +1,79 @@
-/*The program is going to take a selection from a player 
-and play it against the computer; depending on the winner,
-the program will update the corresponding lifes; the program
-ends when one of players has no more lifes.*/
+const computerHand = function() {
+  const hand = ['ROCK', 'PAPER', 'SCISSORS'];
+  return hand[Math.floor(Math.random() * hand.length)];
+};
 
-// Initialize player's lifes
+function decideBestHand(player, computer = computerHand()) {
+  let gameHand;
+
+  if (player === computer) {
+    return gameHand = {player:false, computer:false, message:'It\'s a tie!'};
+  }
+
+  if (player === 'ROCK' && computer === 'SCISSORS' 
+    || player === 'PAPER' && computer === 'ROCK' 
+    || player === 'SCISSORS' && computer === 'PAPER') {
+      return gameHand = {player:true, computer:false, message:`You Win! ${player} beats ${computer}`};
+  } else {
+    return gameHand = {player:false, computer:true, message:`You Lose! ${computer} beats ${player}`};
+  }
+}
+
+function playRound(player) {
+  const roundHand = decideBestHand(player);
+
+  if (roundHand.player) {
+    computerLifes -= 1;
+    resultHand.textContent = `${roundHand.message}`;
+    score.textContent = `PlayerLifes: ${playerLifes} ComputerLifes: ${computerLifes}`;
+  } else if (roundHand.computer){
+    playerLifes -= 1;
+    resultHand.textContent = `${roundHand.message}`;
+    score.textContent = `PlayerLifes: ${playerLifes} ComputerLifes: ${computerLifes}`;
+  } else {
+    resultHand.textContent = `${roundHand.message}`;
+    score.textContent = `PlayerLifes: ${playerLifes} ComputerLifes: ${computerLifes}`;
+  }
+}
+
+function decideWinnerGame() {
+  if (playerLifes === 0 || computerLifes == 0) {
+    const weapons = document.querySelectorAll('.weapon');
+    if (playerLifes) {
+      alert('You Won!');
+    } else {
+      alert('You Lost!');
+    }
+    weapons.forEach(weapon => {
+      weapon.disabled = true;
+    });
+  }
+}
+
+function playGame(e) {
+  const button = document.querySelector(`button[id="${e.target.id}"]`);
+  if (!button) {
+    return
+  }
+  
+  playRound(button.textContent.toUpperCase());
+  decideWinnerGame();
+}
+
+function playAgain(e) {
+  location.reload();
+}
+
 let playerLifes = 5;
-// Initialize computer's lifes
 let computerLifes = 5;
-// Start the game in a loop (while lifes > 0))
-while (playerLifes > 0 && computerLifes > 0) {
-  // Initialize player's hand
-  const playerHand = function() {
-    let hand = prompt('Play your hand');
-    return hand.toUpperCase();      
-  };
-  // Initialize computer's hand
-  const computerHand = function() {
-    const hand = ['ROCK', 'PAPER', 'SCISSORS'];
-    const randomNumber = Math.floor(Math.random()*3);
-    return hand[randomNumber]
-  };
-  /*Compute a decision procedure to determine the best hand {
-    gameHand keeps the best hand
-  }*/
-  function decideBestHand(player = playerHand(), computer = computerHand()) {
-    let gameHand;
-    
-    if (player === computer) {
-      gameHand = {player:false, computer:false, message:'It\'s a tie!'};
-    }
 
-    if (player === 'ROCK' && computer === 'SCISSORS' 
-      || player === 'PAPER' && computer === 'ROCK' 
-      || player === 'SCISSORS' && computer === 'PAPER') {
-        return gameHand = {player:true, computer:false, message:`You Win! ${player} beats ${computer}`};
-    } else {
-      return gameHand = {player:false, computer:true, message:`You Lose! ${computer} beats ${player}`};
-    }
-  }
-  // Update lifes and declare the best hand of the round 
-  function playRound() {
-    const gameHand = decideBestHand()
-    if (gameHand.player) {
-      computerLifes -= 1;
-      return gameHand.message;
-    } else if (gameHand.computer){
-      playerLifes -= 1;
-      return gameHand.message;
-    } else {
-      return gameHand.message;
-    }
-  }
-  console.log(playRound())
-}
-// Decide the winner of the game
-if (playerLifes) {
-  console.log('Great! You won')
-} else {
-  console.log('Oh no! The computer won')
-}
+const message = document.querySelector('.message');
+const score = document.querySelector('.score');
+score.textContent = `PlayerLifes: ${playerLifes} ComputerLifes: ${computerLifes}`;
+const resultHand = document.querySelector('.resultHand');
+resultHand.textContent = 'You haven\'t played yet. Waiting for your choice';
+
+const reset = document.querySelector('button[class="reset"]');
+
+window.addEventListener('click', playGame);
+reset.addEventListener('click', playAgain);
